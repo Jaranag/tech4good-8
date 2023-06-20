0e021c47b3957d581f848bf95077f6d545e4ad66
jaranag <joel.aranag@gmail.com>
2023-06-20 13:00:52 +0200

    added survey and distric models...

--- BackEnd-8/database/migrations/2023_06_20_105202_create_surveys_table.php
+++ BackEnd-8/database/migrations/2023_06_20_105202_create_surveys_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('districts', function (Blueprint $table) {
+        Schema::create('surveys', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description');
-            $table->double('total_average');
-            // $table->int
             $table->timestamps();
         });
     }
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('districts');
+        Schema::dropIfExists('surveys');
     }
 };
